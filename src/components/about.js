824ed9aeb13db550b6475bbc6047ec60c8300471
skrijwan100 <165564445+skrijwan100@users.noreply.github.com>
2024-09-22 19:41:20 +0530

    Update about.js

--- src/components/about.js
+++ src/components/about.js
@@ -6,7 +6,7 @@ export default function about() {
      <div>
         <div className="main mx-4 flex flex-col justify-center text-white my-24">
             <h1 className='font-mono font-bold mb-2 text-5xl'>About Me</h1>
-            <img src="/picofme-2.png" alt="" className='mb-8 h-16 w-16 animate-pulse' />
+            <img src="picofme-2.png" alt="" className='mb-8 h-16 w-16 animate-pulse' />
             <span className='font-mono text-left'>
               
             </span>
